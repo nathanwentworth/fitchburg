95e25c3310d8f0096ff688cfbe97fae8c08a9445
Nathan Wentworth <nwentworth95@gmail.com>
2017-06-27 16:58:25 -0400

    added lightbox base, key commands

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,17 +1,67 @@
-var imageEls = document.querySelectorAll('.images')[0].getElementsByTagName('img');
-var lightbox = document.getElementById('lightbox');
-var lightboxImg = document.getElementById('lightbox-img');
 
-getImages();
+var imageEls;
+var lightbox;
+var lightboxImg;
+var lightboxVis;
+
+var imgIndex;
+var imgLength;
+
+window.addEventListener('load', function () {
+  getImages();
+});
 
 var imageSrcs = []
 function getImages() {
+
+  imageEls = document.querySelectorAll('.images')[0].getElementsByTagName('img');
+  imgLength = imageEls.length;
+  lightbox = document.getElementById('lightbox');
+  lightboxImg = document.getElementById('lightbox-img');
+
+  lightboxVis = lightbox.classList.contains('lightbox-visible');
+  lightbox.addEventListener('click', function () {
+    lightboxToggle();
+  });
+
   for (var i = 0; i < imageEls.length; i++) {
     imageEls[i].addEventListener('click', function (event) {
-      var index = getIndex(event);
+      imgIndex = getIndex(event);
+      lightboxToggle();
     });
-    // imageSrcs[i] = imageEls[i].src;
+    imageSrcs[i] = imageEls[i].src;
   }
+
+  document.addEventListener('keydown', function (event) {
+    console.log(lightboxVis);
+    console.log(event.keyCode);
+    if (lightboxVis) {
+
+      if (event.keyCode == 27) {
+        lightboxToggle();
+        return;
+      }
+
+      if (event.keyCode == 37 || event.keyCode == 65) {
+        imgIndex--;
+        if (imgIndex < 0) {
+          imgIndex = imgLength - 1;
+        }
+
+      } else if (event.keyCode == 39 || event.keyCode == 68) {
+        imgIndex++;
+        if (imgIndex >= imgLength) {
+          imgIndex = 0;
+        }
+      }
+
+      console.log(imgIndex);
+
+      setLightboxImage(imgIndex);
+    }
+  });
+
+  console.log('images loaded');
 }
 
 function getIndex(event) {
@@ -23,4 +73,23 @@ function getIndex(event) {
       return i;
     }
   }
+}
+
+function lightboxToggle() {
+  if (lightboxVis) {
+    lightbox.classList.add('lightbox-hidden');
+    lightbox.classList.remove('lightbox-visible');
+    lightboxVis = false;
+  } else {
+    lightbox.classList.add('lightbox-visible');
+    lightbox.classList.remove('lightbox-hidden');
+
+    lightboxVis = true;
+
+    setLightboxImage();
+  }
+}
+
+function setLightboxImage() {
+  lightboxImg.src = imageSrcs[imgIndex];
 }
